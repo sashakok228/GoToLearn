c16b6ba9647c18d64e6e987edd126a75603b0cc1
sasha <sasha.antipin6794@mail.ru>
2026-06-03 05:23:40 +0600

    Переведено всё на русский 2 0

--- app/src/main/java/com/example/exammaster/network/ApiConfig.java
+++ app/src/main/java/com/example/exammaster/network/ApiConfig.java
@@ -1,5 +1,5 @@
 package com.example.exammaster.network;
 
 public class ApiConfig {
-    public static final String BASE_URL = "http://10.0.2.2:8081";
+    public static final String BASE_URL = "https://earthly-litigate-pesticide.ngrok-free.dev";
 }
